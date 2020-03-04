84beec534a752adee2cebda0f914885043deefe9
Josephine Kvarnberg <jkv@kth.se>
2020-03-04 16:55:26 +0100

    search updated

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "../../App.css";
 
-function Search() {
-  return <div className="Search">searching</div>;
+class Search extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      clicked: false
+    };
+  }
+
+  doThis = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
+    return (
+      <div className="Search">
+        <button onClick={this.doThis}>Dogs</button>
+        <div>{this.state.clicked ? <h1>dogs clicked</h1> : null}</div>
+      </div>
+    );
+  }
 }
 
 export default Search;
